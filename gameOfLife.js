99a44bab710ab659232c7fdd61ebb9c46f5f8bfb
Sidhant Panda <sidhantpanda@gmail.com>
2018-08-03 15:21:41 +0530

    complete with stats

---
+++ gameOfLife.js
@@ -0,0 +1,202 @@
+const DraftLog = require('draftlog');
+DraftLog(console);
+
+const DEAD = ' ⚪️ ';
+const LIVE = ' 🔴 ';
+
+function GameOfLife() {
+  return {
+    rows: 0,
+    columns: 0,
+    init: function (rows, columns) {
+      this.rows = rows;
+      this.columns = columns;
+      this.board = getRandomBoard(rows, columns);
+      this.displayMatrix = [];
+      for (let i = 0; i < rows; i++) {
+        this.displayMatrix.push(console.draft());
+      }
+
+      this.statsDisplayMatrix['alive'] = console.draft();
+      this.statsDisplayMatrix['dead'] = console.draft();
+      this.statsDisplayMatrix['kills'] = console.draft();
+      this.statsDisplayMatrix['births'] = console.draft();
+
+      this.updateCurrentStats();
+    },
+    board: [],
+    displayMatrix: [],
+    hasChanged: true,
+    stats: {
+      alive: 0,
+      dead: 0,
+      kills: 0,
+      births: 0
+    },
+    statsDisplayMatrix: [],
+    tick: function () {
+      let hasChanged = false;
+      let matrixCopy = JSON.parse(JSON.stringify(this.board));
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.columns; j++) {
+          const info = this.getNeighbourInfo(i, j);
+          if (this.board[i][j] === true) {
+            if (info.live < 2) {
+              matrixCopy[i][j] = false;
+              this.stats.kills++;
+              hasChanged = true;
+            } else if (info.live > 3) {
+              matrixCopy[i][j] = false;
+              this.stats.kills++;
+              hasChanged = true;
+            }
+          } else {
+            if (info.live === 3) {
+              matrixCopy[i][j] = true;
+              this.stats.births++;
+              hasChanged = true;
+            }
+          }
+        }
+      }
+
+      this.hasChanged = hasChanged;
+      this.updateCurrentStats();
+      this.board = matrixCopy;
+    },
+    printBoard: function () {
+      for (let i = 0; i < this.rows; i++) {
+        const toPrint = this.board[i].reduce((acc, val) => {
+          if (acc === false) {
+            acc = DEAD;
+          } else if (acc === true) {
+            acc = LIVE;
+          }
+          if (val === false) {
+            return acc + DEAD;
+          } else {
+            return acc + LIVE;
+          }
+        });
+        this.displayMatrix[i](toPrint);
+      }
+    },
+    printStats: function () {
+      this.statsDisplayMatrix['alive']('Alive: ' + this.stats.alive);
+      this.statsDisplayMatrix['dead']('Dead: ' + this.stats.dead);
+      this.statsDisplayMatrix['kills']('Kills: ' + this.stats.kills);
+      this.statsDisplayMatrix['births']('Births: ' + this.stats.births);
+    },
+    updateCurrentStats: function () {
+      this.stats.alive = 0;
+      this.stats.dead = 0;
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.columns; j++) {
+          if (this.board[i][j]) {
+            this.stats.alive++;
+          } else {
+            this.stats.dead++;
+          }
+        }
+      }
+    },
+    getNeighbourInfo: function (row, column) {
+      const info = {
+        live: 0,
+        dead: 0
+      }
+
+      if (row > 0) {
+        if (this.board[row - 1][column] === false) {
+          info.dead++;
+        } else {
+          info.live++;
+        }
+      }
+
+      if (column > 0) {
+        if (this.board[row][column - 1] === false) {
+          info.dead++;
+        } else {
+          info.live++;
+        }
+      }
+
+      if (row < this.rows - 1) {
+        if (this.board[row + 1][column] === false) {
+          info.dead++;
+        } else {
+          info.live++;
+        }
+      }
+
+      if (column < this.columns - 1) {
+        if (this.board[row][column + 1] === false) {
+          info.dead++;
+        } else {
+          info.live++;
+        }
+      }
+
+      if (row > 0 && column > 0) {
+        if (this.board[row - 1][column - 1] === false) {
+          info.dead++;
+        } else {
+          info.live++;
+        }
+      }
+
+      if (row > 0 && column < this.columns - 1) {
+        if (this.board[row - 1][column + 1] === false) {
+          info.dead++;
+        } else {
+          info.live++;
+        }
+      }
+
+      if (row < this.rows - 1 && column < this.columns - 1) {
+        if (this.board[row + 1][column + 1] === false) {
+          info.dead++;
+        } else {
+          info.live++;
+        }
+      }
+
+      if (row < this.rows - 1 && column > 0) {
+        if (this.board[row + 1][column - 1] === false) {
+          info.dead++;
+        } else {
+          info.live++;
+        }
+      }
+
+      return info;
+    }
+  }
+}
+
+function getRandomBoard(rows, columns) {
+  const board = [];
+  for (let i = 0; i < rows; i++) {
+    board.push(getRow(columns));
+  }
+  return board;
+}
+
+function getRow(columns) {
+  const row = [];
+  for (let i = 0; i < columns; i++) {
+    if (getRandomNumber(0, 1) === 0) {
+      row.push(false);
+    } else {
+      row.push(true);
+    }
+  }
+  return row;
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+module.exports = GameOfLife;
--- index.js
+++ index.js
@@ -1,167 +1,23 @@
-const DraftLog = require('draftlog');
-DraftLog(console);
+const GameOfLife = require('./gameOfLife');
+const game = new GameOfLife();
 
 let TOTAL_ROWS = 20;
 let TOTAL_COLUMNS = 16;
-const DEAD = ' ⚪️ ';
-const LIVE = ' 🔴 ';
-let interval = null;
-
 var args = process.argv.slice(2);
 if (args.length > 1) {
   TOTAL_ROWS = parseInt(args[0]);
   TOTAL_COLUMNS = parseInt(args[1]);
 }
 
-let GAME_MATRIX = [];
-const DISPLAY_MATRIX = [];
-let iterationNumber = 0;
-
-for (let i = 0; i < TOTAL_ROWS; i++) {
-  GAME_MATRIX.push(getRow());
-  DISPLAY_MATRIX.push(console.draft());
-}
-
-let currentIteration = 0;
-iterationLog = console.draft();
-
-printGameMatrix();
+game.init(TOTAL_ROWS, TOTAL_COLUMNS);
+game.printBoard();
+game.printStats();
 
 interval = setInterval(() => {
-  currentIteration++;
-  transitionToNextPhase();
-  printGameMatrix();
-}, 200);
-
-function transitionToNextPhase() {
-  let hasChanged = false;
-  let matrixCopy = JSON.parse(JSON.stringify(GAME_MATRIX));
-  // Any live cell with fewer than two live neighbors dies, as if by under population.
-  // Any live cell with two or three live neighbors lives on to the next generation.
-  // Any live cell with more than three live neighbors dies, as if by overpopulation.
-  // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-  for (let row = 0; row < TOTAL_ROWS; row++) {
-    const thisRow = [];
-    for (let column = 0; column < TOTAL_COLUMNS; column++) {
-      const info = getNeighbourInfo(matrixCopy, row, column);
-      if (matrixCopy[row][column] === LIVE) {
-        if (info.live < 2) {
-          GAME_MATRIX[row][column] = DEAD;
-          hasChanged = true;
-        } else if (info.live > 3) {
-          GAME_MATRIX[row][column] = DEAD;
-          hasChanged = true;
-        }
-      } else {
-        if (info.live === 3) {
-          GAME_MATRIX[row][column] = LIVE;
-          hasChanged = true;
-        }
-      }
-      thisRow.push
-    }
-  }
-
-  if (!hasChanged) {
-    console.log('FIN!');
+  game.tick();
+  if (!game.hasChanged) {
     clearInterval(interval);
   }
-}
-
-function getNeighbourInfo(matrix, row, column) {
-  const info = {
-    live: 0,
-    dead: 0
-  }
-
-  if (row > 0) {
-    if (matrix[row - 1][column] === DEAD) {
-      info.dead++;
-    } else {
-      info.live++;
-    }
-  }
-
-  if (column > 0) {
-    if (matrix[row][column - 1] === DEAD) {
-      info.dead++;
-    } else {
-      info.live++;
-    }
-  }
-
-  if (row < TOTAL_ROWS - 1) {
-    if (matrix[row + 1][column] === DEAD) {
-      info.dead++;
-    } else {
-      info.live++;
-    }
-  }
-
-  if (column < TOTAL_COLUMNS - 1) {
-    if (matrix[row][column + 1] === DEAD) {
-      info.dead++;
-    } else {
-      info.live++;
-    }
-  }
-
-  if (row > 0 && column > 0) {
-    if (matrix[row - 1][column - 1] === DEAD) {
-      info.dead++;
-    } else {
-      info.live++;
-    }
-  }
-
-  if (row > 0 && column < TOTAL_COLUMNS - 1) {
-    if (matrix[row - 1][column + 1] === DEAD) {
-      info.dead++;
-    } else {
-      info.live++;
-    }
-  }
-
-  if (row < TOTAL_ROWS - 1 && column < TOTAL_COLUMNS - 1) {
-    if (matrix[row + 1][column + 1] === DEAD) {
-      info.dead++;
-    } else {
-      info.live++;
-    }
-  }
-
-  if (row < TOTAL_ROWS - 1 && column > 0) {
-    if (matrix[row + 1][column - 1] === DEAD) {
-      info.dead++;
-    } else {
-      info.live++;
-    }
-  }
-
-  return info;
-}
-
-function printGameMatrix() {
-  const reducer = (acc, val) => acc + val;
-  for (let i = 0; i < TOTAL_ROWS; i++) {
-    const toPrint = GAME_MATRIX[i].reduce(reducer);
-    DISPLAY_MATRIX[i](toPrint);
-  }
-  iterationLog('Iteration # ' + currentIteration);
-}
-
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRow() {
-  const row = [];
-  for (let i = 0; i < TOTAL_COLUMNS; i++) {
-    if (getRandomNumber(0, 1) === 0) {
-      row.push(DEAD);
-    } else {
-      row.push(LIVE);
-    }
-  }
-  return row;
-}
+  game.printBoard();
+  game.printStats();
+}, 300);
